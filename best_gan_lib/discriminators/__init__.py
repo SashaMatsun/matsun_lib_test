e8a319220d28aebeb7be93d5d7018fb514b20f03
Merrah <ienjoyeatingcorpses@gmail.com>
2021-04-03 16:30:54 +0300

    added SGAN (discr only as gen is same as DCGAN)

--- best_gan_lib/discriminators/__init__.py
+++ best_gan_lib/discriminators/__init__.py
@@ -2,3 +2,4 @@ from .dcgan import DCGANDiscriminator
 from .cgan import CGANDiscriminator
 from .pix2pix import P2PDiscriminator
 from .infogan import InfoGANDiscriminator
+from .sgan import SGANDiscriminator
