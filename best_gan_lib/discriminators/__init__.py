90fecc421ddb82840c02613dba38972385652a30
Merrah <ienjoyeatingcorpses@gmail.com>
2021-04-14 01:38:36 +0300

    minor changes2

--- best_gan_lib/discriminators/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dcgan import DCGANDiscriminator
-from .cgan import CGANDiscriminator
-from .pix2pix import P2PDiscriminator
-from .infogan import InfoGANDiscriminator
-from .sgan import SGANDiscriminator
-from .began import BEGANDiscriminator
-from .sagan import SAGANDiscriminator
--- best_gan_lib/model.py
+++
@@ -1,4 +0,0 @@
-class Model():
-    def __init__(self, g, d):
-        self.discr = d
-        self.gen = g
